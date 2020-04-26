74829748a5eb594ecf34dc5c08ca98ecb3bcdfe1
Patrick Basquel <pbdevpros@gmail.com>
2020-04-26 13:42:07 +0000

    ! input error in test_slam.sh

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 home="/home/workspace/"
-if [ -z $1 ]
+if [ ! -z $1 ]
 then
-	$home=$1
+	home=$1
 fi
 
-xterm -e " source $homecatkin_ws/devel/setup.bash ; roslaunch ros_robot world.launch " &
+xterm -e " source ${home}catkin_ws/devel/setup.bash ; cd $homecatkin_ws ; roslaunch ros_robot world.launch " &
 sleep 5
-xterm -e " source $homecatkin_ws/devel/setup.bash ; roslaunch ros_robot mapping.launch " &
+xterm -e " source ${home}catkin_ws/devel/setup.bash ; cd $homecatkin_ws ; roslaunch ros_robot mapping.launch " &
 sleep 5
-xterm -e " source $homecatkin_ws/devel/setup.bash ; roslaunch turtlebot_teleop keyboard_teleop.launch " & # launch teleop keyboard control for robot
+xterm -e " source ${home}catkin_ws/devel/setup.bash ; roslaunch turtlebot_teleop keyboard_teleop.launch " & 		# launch teleop keyboard control for robot
 sleep 5
-# xterm -e " source $homecatkin_ws/devel/setup.bash ; roslaunch gmapping gmapping_demo.launch " 
-xterm -e " source $homecatkin_ws/devel/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch " & # preconfigured rviz workspace
+xterm -e " source ${home}catkin_ws/devel/setup.bash ; rosrun gmapping slam_gmapping scan:=scan " & 			# gmapping service
+sleep 5
+xterm -e " source ${home}catkin_ws/devel/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch " & 	# preconfigured rviz workspace
 sleep 5
 
